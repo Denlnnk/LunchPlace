d741db21827e11e2cad556286bd53d1dc86af9de
denis <denlnnk2014@gmail.com>
2022-09-25 14:52:42 +0300

    Changed path

--- lunchplace/api/service/restaurant_upload_service.py
+++ lunchplace/api/service/restaurant_upload_service.py
@@ -5,13 +5,13 @@ from os import listdir
 
 def upload(request_data):
     """Uploading data from Restaurant files"""
-    only_files = [f for f in listdir(str(pathlib.Path().absolute()) + '/api/static/restaurants')]
+    only_files = [f for f in listdir(str(pathlib.Path().absolute()) + '/lunchplace/api/static/restaurants')]
     list_names = [names.rstrip('.json') for names in only_files]
     data = []
     requested_day = request_data['day']
 
     for restaurant_name in list_names:
-        file = open(str(pathlib.Path().absolute()) + '/api/static/restaurants/' + restaurant_name + '.json')
+        file = open(str(pathlib.Path().absolute()) + '/lunchplace/api/static/restaurants/' + restaurant_name + '.json')
         restaurant_info = json.load(file)
 
         restaurant_info_menu = restaurant_info['menu']
